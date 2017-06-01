899c1cf2d8739017c4736a09f70e2b9b8b653c34
gilbrater <gilbrater1@gmail.com>
2017-06-01 12:56:18 +0100

    test for log out

--- server/models/users.js
+++ server/models/users.js
@@ -60,9 +60,7 @@ UserSchema.methods.removeToken = function(token){
 
     return user.update({
         $pull:{
-            tokens:{
-                token:token
-            }
+            tokens:{token}
         }
     });
 }
